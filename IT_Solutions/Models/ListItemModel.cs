1437e3950473d28a9de0a192adcf4ec4e1ed219c
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-06-15 09:37:51 +0200

    ListItem model added; Layout updated; Services folder added;

---
+++ IT_Solutions/Models/ListItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IT_Solutions.Models
+{
+    public class ListItemModel
+    {
+        public string ImgUrl { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
